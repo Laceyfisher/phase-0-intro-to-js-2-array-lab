de7d31f6194331e61e768580ef0ac525adaf7a0c
Lacey Fisher <lacey.fisher89@gmail.com>
2023-09-10 17:40:05 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name)
+    return cats
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+    return cats
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    const newArray = [...cats]
+    newArray.push(name)
+    return newArray
+}
+function prependCat(name) {
+    const newArray = [...cats]
+    newArray.unshift(name)
+    return newArray
+}
+function removeLastCat() {
+    const newArray = [...cats]
+    newArray.pop()
+    return newArray
+}
+function removeFirstCat() {
+    const newArray = [...cats]
+    newArray.shift()
+    return newArray
+}
